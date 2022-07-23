e48b6576e43f681e1a6238d9f1728c2b306c6ea4
Maria Dunn <mariadunn888@gmail.com>
2022-07-23 21:47:16 +0100

    Pre-deploy commit

--- config.py
+++ config.py
@@ -7,11 +7,12 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
 
     # Image Upload config
 
     MAX_CONTENT_LENGTH = 1024 * 1024 # 1024 = 1mb
     UPLOAD_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.gif']
     UPLOAD_PATH = 'app/static/uploads'
-
-    # Resize config
