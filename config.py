bf75eb5e4c2affcacfd74e905a7efc7cbce4ac47
Maria Dunn <mariadunn888@gmail.com>
2022-07-24 22:13:56 +0100

    Edited Config SQL URI

--- config.py
+++ config.py
@@ -3,9 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' #TODO: change
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or \
